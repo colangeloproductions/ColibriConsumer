a4715103feb9c176e082599283c63559481cb7aa
Romeo Colanglelo <romeo@colangelo.me>
2023-07-25 16:12:35 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import { Heading, Icon } from '@ugg/colibri';
+import packageInfo from '../package.json';
+
 const App = () => {
+  const packageName = '@ugg/colibri';
+  const packageVersion =
+    packageInfo.dependencies && packageInfo.dependencies[packageName];
+
   return (
     <>
-      <Heading>This is my React app using Colibri!</Heading>
+      <Heading>
+        React app using Colibri {packageName}: {packageVersion || 'Not Found'}
+      </Heading>
       <Icon type='Beer' />
     </>
   );
 };
+
 ReactDOM.render(<App />, document.getElementById('app'));
